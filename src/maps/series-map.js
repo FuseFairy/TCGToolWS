919f9d393398ccdb06fa64617f43d441b976edaa
FuseFairy <dd8611706@gmail.com>
2025-09-20 01:48:24 +0800

    feat: add angel beats series icon and update series map

--- src/maps/series-map.js
+++ src/maps/series-map.js
@@ -1,3 +1,52 @@
 export const seriesMap = {
-  'Angel Beats!／クドわふたー': ['ab', 'kw', 'kab', 'hab'],
+  'Angel Beats!／クドわふたー': {
+    id: 'angel-beats',
+    prefixes: ['AB', 'KW', 'Kab', 'Hab'],
+    icon: 'angelbeats.webp',
+  },
+  'Angel Beats!／クドわふたー1': {
+    id: 'angel-beats',
+    prefixes: ['AB', 'KW', 'Kab', 'Hab'],
+    icon: 'angelbeats.webp',
+  },
+  'Angel Beats!／クドわふたー2': {
+    id: 'angel-beats',
+    prefixes: ['AB', 'KW', 'Kab', 'Hab'],
+    icon: 'angelbeats.webp',
+  },
+  'Angel Beats!／クドわふたー3': {
+    id: 'angel-beats',
+    prefixes: ['AB', 'KW', 'Kab', 'Hab'],
+    icon: 'angelbeats.webp',
+  },
+  'Angel Beats!／クドわふたー4': {
+    id: 'angel-beats',
+    prefixes: ['AB', 'KW', 'Kab', 'Hab'],
+    icon: 'angelbeats.webp',
+  },
+  'Angel Beats!／クドわふたー5': {
+    id: 'angel-beats',
+    prefixes: ['AB', 'KW', 'Kab', 'Hab'],
+    icon: 'angelbeats.webp',
+  },
+  'Angel Beats!／クドわふたー6': {
+    id: 'angel-beats',
+    prefixes: ['AB', 'KW', 'Kab', 'Hab'],
+    icon: 'angelbeats.webp',
+  },
+  'Angel Beats!／クドわふたー7': {
+    id: 'angel-beats',
+    prefixes: ['AB', 'KW', 'Kab', 'Hab'],
+    icon: 'angelbeats.webp',
+  },
+  'Angel Beats!／クドわふたー8': {
+    id: 'angel-beats',
+    prefixes: ['AB', 'KW', 'Kab', 'Hab'],
+    icon: 'angelbeats.webp',
+  },
+  'Angel Beats!／クドわふたー9': {
+    id: 'angel-beats',
+    prefixes: ['AB', 'KW', 'Kab', 'Hab'],
+    icon: 'angelbeats.webp',
+  },
 }
