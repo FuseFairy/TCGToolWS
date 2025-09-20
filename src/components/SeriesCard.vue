f700ddbaabf0342b9fae5ed3655f9793ea4e3216
FuseFairy <dd8611706@gmail.com>
2025-09-20 21:20:30 +0800

    feat: implement lazy loading for SeriesCard images

--- src/components/SeriesCard.vue
+++ src/components/SeriesCard.vue
@@ -1,28 +1,30 @@
 <template>
   <v-tooltip :text="seriesName" location="top center">
     <template v-slot:activator="{ props }">
-      <v-card v-bind="props" class="series-card d-flex flex-column" hover
-        :to="{ name: 'SeriesDetail', params: { seriesName: seriesData.id } }" variant="flat" color="surface"
-        rounded="lg">
-        <v-img :src="iconUrl" aspect-ratio="1" cover rounded="lg" class="ma-3">
-          <template v-slot:placeholder>
-            <div class="d-flex align-center justify-center fill-height">
-              <v-progress-circular color="grey-lighten-4" indeterminate></v-progress-circular>
+      <v-lazy :options="{ 'threshold': 0.5 }" transition="fade-transition" class="w-100">
+        <v-card v-bind="props" class="series-card d-flex flex-column" hover
+          :to="{ name: 'SeriesDetail', params: { seriesName: seriesData.id } }" variant="flat" color="surface"
+          rounded="lg">
+          <v-img :src="iconUrl" aspect-ratio="1" cover rounded="lg" class="ma-3">
+            <template v-slot:placeholder>
+              <div class="d-flex align-center justify-center fill-height">
+                <v-progress-circular color="grey-lighten-4" indeterminate></v-progress-circular>
+              </div>
+            </template>
+          </v-img>
+
+          <div class="card-content pa-3">
+            <div class="d-flex align-center text-caption text-grey-lighten-1 mb-2 text-truncate-container">
+              <v-icon start size="small">mdi-layers-outline</v-icon>
+              <span class="text-truncate">{{ seriesData.prefixes.join(', ') }}</span>
             </div>
-          </template>
-        </v-img>
 
-        <div class="card-content pa-3">
-          <div class="d-flex align-center text-caption text-grey-lighten-1 mb-2 text-truncate-container">
-            <v-icon start size="small">mdi-layers-outline</v-icon>
-            <span class="text-truncate">{{ seriesData.prefixes.join(', ') }}</span>
+            <p class="text-h6 font-weight-bold text-truncate-container">
+              <span class="text-truncate">{{ seriesName }}</span>
+            </p>
           </div>
-
-          <p class="text-h6 font-weight-bold text-truncate-container">
-            <span class="text-truncate">{{ seriesName }}</span>
-          </p>
-        </div>
-      </v-card>
+        </v-card>
+      </v-lazy>
     </template>
   </v-tooltip>
 </template>
