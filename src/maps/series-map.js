d51e6690229d1b41a5636ab8f0257acb8d016981
FuseFairy <dd8611706@gmail.com>
2025-09-21 16:19:05 +0800

    refactor: add development-only entries

--- src/maps/series-map.js
+++ src/maps/series-map.js
@@ -1,52 +1,21 @@
 export const seriesMap = {
   'Angel Beats!／クドわふたー': {
-    id: 'angel-beats',
+    id: 'angel-beats-0', // 給一個唯一的 ID
     prefixes: ['AB', 'KW', 'Kab', 'Hab'],
     icon: 'angelbeats.webp',
   },
-  'Angel Beats!／クドわふたー1': {
-    id: 'angel-beats',
-    prefixes: ['AB', 'KW', 'Kab', 'Hab'],
-    icon: 'angelbeats.webp',
-  },
-  'Angel Beats!／クドわふたー2': {
-    id: 'angel-beats',
-    prefixes: ['AB', 'KW', 'Kab', 'Hab'],
-    icon: 'angelbeats.webp',
-  },
-  'Angel Beats!／クドわふたー3': {
-    id: 'angel-beats',
-    prefixes: ['AB', 'KW', 'Kab', 'Hab'],
-    icon: 'angelbeats.webp',
-  },
-  'Angel Beats!／クドわふたー4': {
-    id: 'angel-beats',
-    prefixes: ['AB', 'KW', 'Kab', 'Hab'],
-    icon: 'angelbeats.webp',
-  },
-  'Angel Beats!／クドわふたー5': {
-    id: 'angel-beats',
-    prefixes: ['AB', 'KW', 'Kab', 'Hab'],
-    icon: 'angelbeats.webp',
-  },
-  'Angel Beats!／クドわふたー6': {
-    id: 'angel-beats',
-    prefixes: ['AB', 'KW', 'Kab', 'Hab'],
-    icon: 'angelbeats.webp',
-  },
-  'Angel Beats!／クドわふたー7': {
-    id: 'angel-beats',
-    prefixes: ['AB', 'KW', 'Kab', 'Hab'],
-    icon: 'angelbeats.webp',
-  },
-  'Angel Beats!／クドわふたー8': {
-    id: 'angel-beats',
-    prefixes: ['AB', 'KW', 'Kab', 'Hab'],
-    icon: 'angelbeats.webp',
-  },
-  'Angel Beats!／クドわふたー9': {
-    id: 'angel-beats',
-    prefixes: ['AB', 'KW', 'Kab', 'Hab'],
-    icon: 'angelbeats.webp',
-  },
+}
+
+const isDevelopment = import.meta.env.DEV
+
+if (isDevelopment) {
+  for (let i = 1; i <= 100; i++) {
+    const key = `Angel Beats!／クドわふたー${i}`
+
+    seriesMap[key] = {
+      id: `angel-beats-${i}`,
+      prefixes: ['AB', 'KW', 'Kab', 'Hab'],
+      icon: 'angelbeats.webp',
+    }
+  }
 }
