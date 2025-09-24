ff86420975916c88c3ca59b3e1a481ed05ddea55
FuseFairy <dd8611706@gmail.com>
2025-09-24 21:15:45 +0800

    feat: implement dual sidebar layout

--- src/components/SidebarLayout.vue
+++ src/components/SidebarLayout.vue
@@ -1,7 +1,6 @@
 <template>
-  <aside class="overflow-y w-25 pl-4 pb-4 d-flex flex-column flex-shrink-0"
-    :style="{ paddingTop: `${headerOffsetHeight + 18}px` }">
-    <v-sheet rounded="lg" class="pa-4 d-flex flex-column ga-4 flex-grow-1">
+  <aside class="d-flex flex-column flex-shrink-0" :style="{ paddingTop: `${headerOffsetHeight + 18}px` }">
+    <v-sheet rounded="lg" class="pa-4 ga-4 d-flex flex-column flex-grow-1 overflow-y-auto themed-scrollbar">
       <slot></slot>
       <v-spacer class="fill-height"></v-spacer>
     </v-sheet>
