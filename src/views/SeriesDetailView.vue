d4ddc8058d1ef186c3f46e6c845e62048763e1b0
FuseFairy <dd8611706@gmail.com>
2025-09-24 21:15:45 +0800

    feat: implement dual sidebar layout

--- src/views/SeriesDetailView.vue
+++ src/views/SeriesDetailView.vue
@@ -9,27 +9,40 @@
     </div>
 
     <div v-else class="d-flex flex-column h-100">
+
       <div ref="headerRef" class="overlay-header pa-4 pb-0 pt-0">
-        <div class="d-flex align-center">
-          <v-btn :size="chipSize" icon="mdi-arrow-left" variant="text" :to="{ name: 'SeriesCardTable' }"
-            class="mr-2"></v-btn>
+        <div class="d-flex align-center justify-space-between w-100">
 
-          <h1 class="text-h6 text-sm-h4 text-truncate flex-grow-1 text-center px-2">{{ seriesName }}</h1>
+          <v-btn :size="resize" :icon="filterIcon" variant="text" @click="isFilterOpen = !isFilterOpen"></v-btn>
 
-          <v-chip :size="chipSize" prepend-icon="mdi-cards-outline" class="counter-chip font-weight-bold">
-            {{ cards.length }}
-          </v-chip>
+          <div class="d-flex align-center">
+            <v-btn :size="resize" icon="mdi-arrow-left" variant="text" :to="{ name: 'SeriesCardTable' }"></v-btn>
+            <h1 class="text-h6 text-sm-h4 text-truncate text-center px-2">{{ seriesName }}</h1>
+            <v-chip :size="resize" prepend-icon="mdi-cards-diamond-outline" class="counter-chip font-weight-bold">
+              {{ cards.length }}
+            </v-chip>
+          </div>
+
+          <v-btn :size="resize" icon="mdi-cards" variant="text" @click="isCardDeckOpen = !isCardDeckOpen"></v-btn>
         </div>
       </div>
 
       <div class="d-flex flex-row overflow-hidden">
-        <SidebarLayout :header-offset-height="headerOffsetHeight">
-          <v-select label="分類" :items="['A', 'B', 'C']" hide-details></v-select>
-          <v-text-field label="關鍵字" hide-details></v-text-field>
-          <v-checkbox label="僅顯示收藏" hide-details></v-checkbox>
-        </SidebarLayout>
+        <div class="sidebar-container" :class="{ 'left-sidebar-open': isFilterOpen }">
+          <SidebarLayout class="fill-height pl-4 pb-4" :header-offset-height="headerOffsetHeight">
+            <v-select label="分類" :items="['A', 'B', 'C']" hide-details></v-select>
+            <v-text-field label="關鍵字" hide-details></v-text-field>
+            <v-checkbox label="僅顯示收藏" hide-details></v-checkbox>
+          </SidebarLayout>
+        </div>
+
         <CardInfiniteScrollList :cards="cards" :header-offset-height="headerOffsetHeight" empty-text="" margin="300"
           class="flex-grow-1 themed-scrollbar pl-4 pr-4" />
+
+        <div class="sidebar-container" :class="{ 'right-sidebar-open': isCardDeckOpen }">
+          <SidebarLayout class="fill-height pr-4 pl-4 pb-4" :header-offset-height="headerOffsetHeight">
+          </SidebarLayout>
+        </div>
       </div>
     </div>
   </v-container>
@@ -51,12 +64,15 @@ const props = defineProps({
 })
 
 const { smAndUp } = useDisplay();
-const chipSize = computed(() => {
+const resize = computed(() => {
   return smAndUp.value ? 'x-large' : 'small';
 });
 
 const headerRef = ref(null);
 const rawHeaderHeight = ref(0);
+const isCardDeckOpen = ref(false);
+const isFilterOpen = ref(false);
+const filterIcon = computed(() => isFilterOpen.value ? 'mdi-filter-off' : 'mdi-filter');
 const headerOffsetHeight = computed(() => rawHeaderHeight.value);
 
 const observer = new ResizeObserver(([entry]) => {
@@ -83,3 +99,20 @@ onUnmounted(() => {
   observer.disconnect();
 });
 </script>
+
+<style scoped>
+.sidebar-container {
+  width: 0;
+  transition: width 0.3s ease;
+  overflow: hidden;
+  flex-shrink: 0;
+}
+
+.sidebar-container.left-sidebar-open {
+  width: 15%;
+}
+
+.sidebar-container.right-sidebar-open {
+  width: 25%;
+}
+</style>
