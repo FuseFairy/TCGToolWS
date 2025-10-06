921e30861c2857b77133a997929c83ccceacb515
FuseFairy <dd8611706@gmail.com>
2025-10-06 22:45:48 +0800

    style: improve filter sidebar UI

--- src/components/FilterSidebar.vue
+++ src/components/FilterSidebar.vue
@@ -4,36 +4,39 @@
     <v-sheet :rounded="smAndUp ? '3md' : false"
       class="pa-4 ga-4 d-flex flex-column fill-height overflow-y-auto themed-scrollbar">
       <div class="d-flex flex-column ga-4">
-        <v-text-field label="关键字" hide-details clearable v-model="filterStore.keyword"></v-text-field>
+        <v-text-field label="关键字" placeholder="卡号、卡名、效果" hide-details clearable v-model="filterStore.keyword"
+          variant="underlined"></v-text-field>
 
         <v-divider></v-divider>
 
-        <v-select label="卡片种类"
+        <v-select label="种类"
           :items="[{ title: '角色', value: '角色卡' }, { title: '事件', value: '事件卡' }, { title: '高潮卡', value: '高潮卡' }]"
-          hide-details multiple chips clearable v-model="filterStore.selectedCardTypes"></v-select>
+          hide-details multiple chips clearable v-model="filterStore.selectedCardTypes" variant="outlined"></v-select>
 
         <v-select label="颜色"
           :items="[{ title: '黄', value: '黄色' }, { title: '绿', value: '绿色' }, { title: '红', value: '红色' }, { title: '蓝', value: '蓝色' }]"
-          hide-details multiple chips clearable v-model="filterStore.selectedColors"></v-select>
+          hide-details multiple chips clearable v-model="filterStore.selectedColors" variant="outlined"></v-select>
 
-        <v-divider></v-divider>
-
-        <v-select label="产品名称" :items="filterStore.productNames" hide-details clearable
-          v-model="filterStore.selectedProductName"></v-select>
+        <v-select label="产品" :items="filterStore.productNames" hide-details clearable
+          v-model="filterStore.selectedProductName" variant="outlined"></v-select>
 
         <v-select label="特征" :items="filterStore.traits" hide-details multiple chips clearable
-          v-model="filterStore.selectedTraits"></v-select>
-
-        <v-divider></v-divider>
+          v-model="filterStore.selectedTraits" variant="outlined"></v-select>
 
         <v-select label="等级" :items="['0', '1', '2', '3']" hide-details multiple chips clearable
-          v-model="filterStore.selectedLevels"></v-select>
+          v-model="filterStore.selectedLevels" variant="outlined"></v-select>
 
-        <v-range-slider label="费用" class="mt-6" hide-details thumb-label="always" :min="filterStore.costRange.min"
-          :max="filterStore.costRange.max" step="1" v-model="filterStore.selectedCostRange"></v-range-slider>
+        <div>
+          <div class="text-caption text-disabled">费用</div>
+          <v-range-slider hide-details thumb-label="true" :min="filterStore.costRange.min"
+            :max="filterStore.costRange.max" step="1" v-model="filterStore.selectedCostRange"></v-range-slider>
+        </div>
 
-        <v-range-slider label="战斗力" class="mt-6" hide-details thumb-label="always" :min="filterStore.powerRange.min"
-          :max="filterStore.powerRange.max" step="500" v-model="filterStore.selectedPowerRange"></v-range-slider>
+        <div>
+          <div class="text-caption text-disabled">战斗力</div>
+          <v-range-slider hide-details thumb-label="true" :min="filterStore.powerRange.min"
+            :max="filterStore.powerRange.max" step="500" v-model="filterStore.selectedPowerRange"></v-range-slider>
+        </div>
       </div>
     </v-sheet>
   </aside>
