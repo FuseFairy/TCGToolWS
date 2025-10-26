feded279c8923f9a23796475830ada1ff6ef97c2
FuseFairy <dd8611706@gmail.com>
2025-10-26 21:02:53 +0800

    refactor: rename cardPrefix to cardIDPrefix for clarity

--- src/composables/useCardImage.js
+++ src/composables/useCardImage.js
@@ -2,9 +2,9 @@ import { computed, unref } from 'vue'
 
 const imageBaseUrl = import.meta.env.VITE_IMAGE_BASE_URL
 
-export const useCardImage = (cardPrefix, cardId) => {
+export const useCardImage = (cardIDPrefix, cardId) => {
   const imageUrl = computed(() => {
-    const cardPrefixValue = unref(cardPrefix)
+    const cardPrefixValue = unref(cardIDPrefix)
     const cardIdValue = unref(cardId)
 
     if (!cardIdValue || typeof cardIdValue !== 'string') {
