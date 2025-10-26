75da9f86eee565e94ad973397e7d834230e60377
FuseFairy <dd8611706@gmail.com>
2025-10-26 21:24:28 +0800

    feat: add deck code input for navigating to shared decks

--- src/views/DecksView.vue
+++ src/views/DecksView.vue
@@ -1,6 +1,20 @@
 <template>
   <div class="fill-height d-flex overflow-y-auto themed-scrollbar">
     <v-container class="h-100 pa-0">
+      <v-row justify="center" class="ma-0 pt-3">
+        <v-col cols="12" sm="8" md="6">
+          <v-text-field
+            v-model="deckCode"
+            label="输入卡组代码"
+            variant="solo"
+            density="compact"
+            append-inner-icon="mdi-magnify"
+            hide-details
+            @keydown.enter="navigateToSharedDeck"
+            @click:append-inner="navigateToSharedDeck"
+          />
+        </v-col>
+      </v-row>
       <v-row class="ma-0 pt-3">
         <v-col v-if="localDeck" cols="6" sm="4" md="3">
           <DeckCard :deck="localDeck" deckKey="local" />
@@ -15,18 +29,31 @@
 
 <script setup>
 import { ref, onMounted, watch, computed } from 'vue'
+import { useRouter } from 'vue-router'
 import { useDeckStore } from '@/stores/deck'
 import { useDeckEncoder } from '@/composables/useDeckEncoder'
 import DeckCard from '@/components/deck/DeckCard.vue'
 import { useUIStore } from '@/stores/ui'
 import { useSnackbar } from '@/composables/useSnackbar'
 
+const router = useRouter()
 const deckStore = useDeckStore()
 const { decodeDeck } = useDeckEncoder()
 const uiStore = useUIStore()
 const { triggerSnackbar } = useSnackbar()
 
+const deckCode = ref('')
 const decodedDecks = ref({})
+
+const navigateToSharedDeck = () => {
+  const code = deckCode.value.trim()
+  if (code) {
+    router.push({
+      name: 'ShareDeckDetail',
+      params: { key: code },
+    })
+  }
+}
 
 const localDeck = computed(() => {
   if (deckStore.totalCardCount > 0) {
