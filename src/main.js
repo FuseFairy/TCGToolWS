14bb2759de578c11d1f1a8e2076d19a9d1b95733
Yuuzi261 <lokasu0531@gmail.com>
2025-10-27 21:46:19 +0800

    fix: improve service worker registration logging & format code

--- src/main.js
+++ src/main.js
@@ -21,14 +21,15 @@ const bootstrap = async () => {
   // 註冊 Service Worker
   if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
-      navigator.serviceWorker.register('/sw.js')
-        .then(registration => {
-          console.log('Service Worker 註冊成功: ', registration);
+      navigator.serviceWorker
+        .register('/sw.js')
+        .then(() => {
+          console.log('Service Worker registration successful!')
         })
-        .catch(registrationError => {
-          console.log('Service Worker 註冊失敗: ', registrationError);
-        });
-    });
+        .catch((registrationError) => {
+          console.log('Service Worker registration failed:', registrationError)
+        })
+    })
   }
 
   const app = createApp(App)
